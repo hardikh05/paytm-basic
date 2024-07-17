8ba2fe771404954119589fc214790f022e99db30
Hardik <hardiksharma05032004@gmail.com>
2024-07-17 17:01:58 +0530

    Add initial backend files

---
+++ db.js
@@ -0,0 +1,29 @@
+const mongoose=require("mongoose");
+
+mongoose.connect("mongodb+srv://hardikh05:qHBMaSAT5KuXuLrH@cluster0.4nficdg.mongodb.net/paytm")
+ 
+const userSchema=mongoose.Schema({
+username:{type:String,
+    unique:true
+},
+password:String,
+firstName:String,
+lastName:String
+
+
+})
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+const Model=mongoose.model('Data',userSchema);
+const Account = mongoose.model('Account', accountSchema);
+module.exports={Model,Account};
